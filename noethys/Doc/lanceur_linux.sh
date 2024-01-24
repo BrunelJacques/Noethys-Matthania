54ce0d8913779fe1516cff34a4f57defc0c7dad0
BrunelJacques <jbrunel@laposte.net>
2024-01-24 22:28:39 +0100

    ajustements pour python 10 / ubuntu

--- noethys/Doc/lanceur_linux.sh
+++ noethys/Doc/lanceur_linux.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-source /home/noegest/envnoethys/bin/activate
-python3 -m Noethys.py
+cd /home/noegest/Noethys-Matthania
+source /home/noegest/Noethys-Matthania/venv/bin/activate
+cd ./noethys
+python3 -m Noethys
 read -n1 -r -p "Tapez une touche pour finir..."
