7ed07445a75951d5d12d7033fdd7272861e4f15a
BrunelJacques <jbrunel@laposte.net>
2024-10-16 03:28:52 +0200

    modification refactoring

---
+++ noethys/Olv/init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# les paths Olv ont été précisé dans les modules
+# fichier laissé vide pour écraser l'ancien qui était de la forme suivante
+"""
+from . ObjectListView import ObjectListView, ColumnDefn
+from . OLVEvent import  CellEditStartedEvent, CellEditStartingEvent
+__all__ = [
+    "CellEditStartedEvent",
+    "CellEditStartingEvent",
+    "ColumnDefn"
+    ]
+"""
